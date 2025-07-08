ec6f9f656d0098b6b016f65c4ea60b36e65751dc
Aman Sharma <149320540+aman-sharma-git@users.noreply.github.com>
2025-07-08 16:59:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
 from jobs_scraper import get_job_listings
 from notion_utils import add_jobs_to_notion
-import datetime
+from datetime import date
 
 if __name__ == "__main__":
     print("🔍 Searching for jobs...")
-    jobs = get_job_listings(limit=15)
-    today = datetime.date.today().isoformat()
+    jobs = get_job_listings()
+    today = date.today().strftime("%Y-%m-%d")
     added = add_jobs_to_notion(jobs, today)
     print(f"✅ Added {added} new jobs to Notion.")
